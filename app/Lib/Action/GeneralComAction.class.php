43fe4f17c86df8024a3eda5e1399775fc3a9ce36
honghui.feihh <13777427154@163.com>
2015-06-23 20:07:47 +0800

    ms

--- app/Lib/Action/GeneralComAction.class.php
+++ app/Lib/Action/GeneralComAction.class.php
@@ -3,16 +3,26 @@
 class GeneralComAction extends Action {
     public function mpbar(){
         header("Content-Type:text/html; charset=utf-8");
-        echo '这里是网页头部mpbar';
+        $info='这里是网页头部mpbar';
+		
+		$this->assign( "info", $info );
+        //输出模板
+        $this->display();
     }
 	
-	public function header(){
+	public function mpheader(){
         header("Content-Type:text/html; charset=utf-8");
-        echo '这里是网页头部header';
+        $info= '这里是网页头部header';
+		$this->assign( "info", $info );
+        //输出模板
+        $this->display();
     }
 	
 	public function mpbottom(){
         header("Content-Type:text/html; charset=utf-8");
-        echo '这里是网页底部mpbottom';
+        $info= '这里是网页底部mpbottom';
+		$this->assign( "info", $info );
+        //输出模板
+        $this->display();
     }
 }
