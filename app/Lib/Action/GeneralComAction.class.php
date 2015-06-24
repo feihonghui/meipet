21e2d4ea088b116ed553581c82a99ac047f5949c
honghui.feihh <13777427154@163.com>
2015-06-24 17:41:02 +0800

    m

--- app/Lib/Action/GeneralComAction.class.php
+++ app/Lib/Action/GeneralComAction.class.php
@@ -2,9 +2,8 @@
 // 通用组件模块
 class GeneralComAction extends Action {
     public function mpbar(){
-        header("Content-Type:text/html; charset=utf-8");
+        //header("Content-Type:text/html; charset=utf-8");
         $info='这里是网页头部mpbar';
-		
 		$this->assign( "info", $info );
         //输出模板
         $this->display();
