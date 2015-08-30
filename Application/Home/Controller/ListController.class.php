4933b831745c61af52f63065a07c531e7f60a945
feihonghui <330559934@qq.com>
2015-08-30 15:26:13 +0800

    feihonghui

--- Application/Home/Controller/ListController.class.php
+++ Application/Home/Controller/ListController.class.php
@@ -7,13 +7,42 @@ use Think\Controller;
 class ListController extends Controller {
 	public function index() {
 		header ( "Content-Type:text/html; charset=utf-8" );
+		$this->display();
+	}
+	
+	public function getPet() {
+		header ( "Content-Type:text/html; charset=utf-8" );
 		$category = $_GET ["category"];
 		$area = $_GET ["area"];
 		$city = $_GET ["city"];
 		$page = $_GET ["page"];
 		$size = $_GET ["size"];
-		$this->display();
+		
+		if(empty($page)){
+			$page=1;
+		}
+		if(!empty($size)){
+		    $size=20;
+		}
+		
+		$limit=($page-1)*$size+","+$size;
+		
+		
+		$Dao = M ( "pet" );
+		if(!empty($category)){
+			$condition ['category'] = $category;
+		}
+		if(!empty($area)){
+		   $condition ['area'] = $area;
+		}
+		if(!empty($city)){
+			$condition ['city'] = $city;
+		}
+		
+		$petList = $Dao->where ( $condition )->limit($limit)->select();
+		$data->result = true;
+	    $data->data=$petList;
+		$this->ajaxReturn($data,"JSONP");
 	}
-	
 }
 ?>
