c007e9e810de126702e9924ec078d006e35f3ba0
feihonghui <330559934@qq.com>
2015-08-30 20:00:13 +0800

    feihonghui

--- Application/Home/Controller/StoreController.class.php
+++ Application/Home/Controller/StoreController.class.php
@@ -32,5 +32,33 @@ class StoreController extends Controller {
 		$this->assign('user',$user);
 		$this->display ();
 	}
+	
+	
+	public function index() {
+		header ( "Content-Type:text/html; charset=utf-8" );
+		$userId = $_GET ["userId"];
+		if(empty($userId)){
+			$this->display();
+			return;
+		}
+	
+		$userDao = M ( "user" );
+		$user = $userDao->where("id=".$userId)->find();
+		//echo $userDao->getLastSql();
+		if(empty($user)){
+			$this->display ();
+			return;
+		}
+	
+		$storeDao = M ( "store" );
+		$store=$storeDao->where("user_id=".$userId)->find();
+		if(empty($store)){
+			$this->display ();
+			return;
+		}
+		$this->assign('store',$store);
+		$this->assign('user',$user);
+		$this->display ();
+	}
 }
 ?>
