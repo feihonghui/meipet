5dec50ed869acbc7d42296ba347bb602df585cdd
贺万里 <hewanli@shining3d.com>
2015-09-07 23:08:53 +0800

    登录注册

--- static/js/global/util.js
+++ static/js/global/util.js
@@ -2,39 +2,37 @@ var Meipet = {};
 
 Meipet.util = {};
 
-Meipet.util.paramsToJSON = function(){
-    var $u = location.search.slice(1),
-        r = {};
-    if( !!$u ){
-        $u = $u.split("&");
-    } else {
-        return r;
+
+// 获取地址栏的参数数组
+Meipet.util.getUrlParams = function(){
+    var search = window.location.search ; 
+    // 写入数据字典
+    var tmparray = search.substr(1,search.length).split("&");
+    var paramsArray = new Array; 
+    if( tmparray != null){
+        for(var i = 0;i<tmparray.length;i++){
+            var reg = /[=|^==]/;    // 用=进行拆分，但不包括==
+            var set1 = tmparray[i].replace(reg,'&');
+            var tmpStr2 = set1.split('&');
+            var array = new Array ; 
+            array[tmpStr2[0]] = tmpStr2[1] ; 
+            paramsArray.push(array);
+        }
     }
-    jQuery.each($u, function ( el ){
-        var $n = el.split("=");
-        r[$n[0]] = $n[1];
-    });
-    return r;
+    // 将参数数组进行返回
+    return paramsArray ;     
 };
 
-Meipet.util.jsonToParams = function( obj ){
-    var $l = location.protocol + "//" + location.hostname + location.pathname;
-    if($.isPlainObject(obj) && !$.isEmptyObject(obj)){
-        var $k = _.keys(obj),
-            $v = _.values(obj),
-            $r = "",
-            $max = $k.length - 1;
-
-        _.each($k, function (el, i){
-            $r = $r + el + "=" + $v[i];
-            if( i < $max ){
-                $r += "&";
+// 根据参数名称获取参数值
+Meipet.util.getParamValue = function(name){
+    var paramsArray = Meipet.util.getUrlParams();
+    if(paramsArray != null){
+        for(var i = 0 ; i < paramsArray.length ; i ++ ){
+            for(var  j in paramsArray[i] ){
+                if( j == name ){
+                    return paramsArray[i][j] ; 
+                }
             }
-        });
-        return $l + "?" + $r + location.hash;
-    } else {
-        return $l + location.hash;
-    }
-};
-
-
+        }
+    } 
+}
